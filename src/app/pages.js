8e4c039f55c905bb537f8bdf0b6d99d56a5cff20
wwfernando07 <wwfernando07@gmail.com>
2025-02-20 19:28:45 -0500

    Fix missing homepage

---
+++ src/app/pages.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Soccer Dashboard</h1>
+        <p>Your Next.js project is working!</p>
+      </div>
+    );
+  }
+  
